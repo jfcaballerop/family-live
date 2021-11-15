87d3b5777d53e7b3e08fa58fcab28fb97d8bbd01
jfcaballerop <jfcaballerop@gmail.com>
2021-11-15 21:11:36 +0100

    fix warn

--- src/services/accessAPI.js
+++ src/services/accessAPI.js
@@ -1,32 +1,30 @@
-import { TOKEN_DEMO } from 'configuration'
-import { USER_INFO_KEY } from 'configuration';
-import { db } from 'configuration/firebaseConfig'
-import { doc, collection, getDocs, getDoc } from 'firebase/firestore';
+import { db } from 'configuration/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
 import { compareHash, encrypt } from 'utils/crypt';
 
 
 async function doGPostLogin(props) {
-    const { username, password } = props
-    // const usersCol = collection(db, 'usuarios');
-    // const users = await getDocs(usersCol)
-    // const usersList = users.docs.map(doc => doc.data());
+	const { username, password } = props
+	// const usersCol = collection(db, 'usuarios');
+	// const users = await getDocs(usersCol)
+	// const usersList = users.docs.map(doc => doc.data());
 
-    // const usersList = await getDocs(collection(db, 'usuarios'))
-    const docRef = doc(db, "usuarios", username);
-    const docSnap = await getDoc(docRef);
+	// const usersList = await getDocs(collection(db, 'usuarios'))
+	const docRef = doc(db, "usuarios", username);
+	const docSnap = await getDoc(docRef);
 
 
-    // console.log(usersList);
-    console.log(docSnap.data().password);
-    console.log(encrypt(password));
-    if (docSnap.exists() && compareHash(password, docSnap.data().password)) {
-        console.log("Document data:", docSnap.data());
-        return docSnap.data()
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-        throw new Error('Login Failed!')
-    }
+	// console.log(usersList);
+	console.log(docSnap.data().password);
+	console.log(encrypt(password));
+	if (docSnap.exists() && compareHash(password, docSnap.data().password)) {
+		console.log("Document data:", docSnap.data());
+		return docSnap.data()
+	} else {
+		// doc.data() will be undefined in this case
+		console.log("No such document!");
+		throw new Error('Login Failed!')
+	}
 }
 
 export { doGPostLogin };
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,27 +1,28 @@
 
 
 export function deleteLocalStorage(key) {
-    if (localStorage.getItem(key)) {
-        localStorage.clear(key)
-        return true
-    } else return false
+	if (localStorage.getItem(key)) {
+		localStorage.clear(key)
+		return true
+	} else return false
 
 }
 export function getLocalStorage(key) {
-    const value = JSON.parse(localStorage.getItem(key))
-    const today = new Date().getTime()
+	const value = JSON.parse(localStorage.getItem(key))
+	if (!value) return null
+	const today = new Date().getTime()
 
-    if (today < value.ttl)
-        return value.data
-    else
-        return null
+	if (today < value.ttl)
+		return value.data
+	else
+		return null
 
 }
 
 export function saveLocalStorage(key, data) {
-    const dataSave = {
-        data: data,
-        ttl: new Date().getTime() + (24 * 60 * 60 * 1000) //date + 24h in msec
-    }
-    localStorage.setItem(key, JSON.stringify(dataSave))
+	const dataSave = {
+		data: data,
+		ttl: new Date().getTime() + (24 * 60 * 60 * 1000) //date + 24h in msec
+	}
+	localStorage.setItem(key, JSON.stringify(dataSave))
 }
